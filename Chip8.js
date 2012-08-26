71c9ee7aa6772ff333f96321326509233ac40b59
Tomasz Finc <tomasz@gmail.com>
2012-08-26 03:02:34 -0700

    Adding clearScreen()

--- Chip8.js
+++ Chip8.js
@@ -334,6 +334,12 @@ function clearDisplay() {
     ctx.clearRect ( 0 , 0 , 150 , 150 );
 }
 
+function clearScreen() {
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+    ctx.clearRect(0 , 0 , 64, 32);
+}
+
 function clearStack() {
     for(i = 0; i < 16; i++) {
         stack[i] = 0;
