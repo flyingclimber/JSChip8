ec908332ce3781278ad313e7998a81d1303c548e
Tomasz Finc <tomasz@gmail.com>
2012-08-27 00:22:21 -0700

    Cleaning up spacing in clearScreen() and clearDisplay()

--- Chip8.js
+++ Chip8.js
@@ -338,13 +338,13 @@ function emulateCycle() {
 function clearDisplay() {
     var canvas = document.getElementById('canvas');
     var ctx = canvas.getContext('2d');
-    ctx.clearRect ( 0 , 0 , 150 , 150 );
+    ctx.clearRect(0,0,150,150);
 }
 
 function clearScreen() {
     var canvas = document.getElementById('canvas');
     var ctx = canvas.getContext('2d');
-    ctx.clearRect(0 , 0 , 64, 32);
+    ctx.clearRect(0,0,64,32);
 }
 
 function clearStack() {
