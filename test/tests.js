dfb1c1c38684eb1807ed6402a745fc8833ce5afa
Tomasz Finc <tomasz@gmail.com>
2012-09-13 10:34:06 -0700

    Bad tomasz didn't update the unit tests after last refactoring. No biscuit for me

--- test/tests.js
+++ test/tests.js
@@ -1,7 +1,7 @@
-test( "clearAllRegisters()", function() {
-    chip8_rv[0] = 2;
-    clearAllRegisters();
-    equal(chip8_rv[0], 0, "Reset registers after setting them");
+test( "clearRegisters()", function() {
+    chip8RV[0] = 2;
+    clearRegisters();
+    equal(chip8RV[0], 0, "Reset registers after setting them");
 });
 test("clearMemory()", function() {
     memory[0xFF] = 255;
@@ -13,10 +13,10 @@ test("clearStack()", function() {
     clearStack();
     equal(stack[2], 0, "Clear the stack after setting it");
 });
-test("resetTimers()", function() {
+test("clearTimers()", function() {
     soundTimer = 2;
     delayTimer = 2;
-    resetTimers();
+    clearTimers();
     equal(soundTimer, 0, "Reset sound timer after setting it");
     equal(delayTimer, 0, "Reset delay timer after setting it");
 });
@@ -25,7 +25,7 @@ test("0x00E0", function() {
     memory = new Array();
     memory[0] = '0x00';
     memory[1] = '0xE0';
-    emulateCycle();
+    decodeOpcode();
     equal(drawFlag,true,"Set drawflag after clear screen"); 
 });
 test("0x00EE", function() {
@@ -36,7 +36,7 @@ test("0x00EE", function() {
     sp = 1;
     stack = new Array();
     stack[0] = 2; 
-    emulateCycle();
+    decodeOpcode();
     equal(pc,4,"Return from a subroutine"); 
 });
 test("0x1NNN", function() {
@@ -44,7 +44,7 @@ test("0x1NNN", function() {
     memory = new Array();
     memory[0] = '0x11';
     memory[1] = '0x20';
-    emulateCycle();
+    decodeOpcode();
     equal(pc,288,"Jump to a new address");
 });
 test("0x2NNN", function() {
@@ -54,7 +54,7 @@ test("0x2NNN", function() {
     memory[16] = '0x22';
     sp = 0;
     stack = new Array();
-    emulateCycle();
+    decodeOpcode();
     equal(pc,290,"Jump to a new address");
     equal(stack[sp - 1],15,"Check saved pc in stack");
 });
@@ -63,9 +63,9 @@ test("0x3XNN", function() {
     memory = new Array();
     memory[0] = '0x31';
     memory[1] = '0x20';
-    clearAllRegisters();
-    chip8_rv[1]= 32;
-    emulateCycle();
+    clearRegisters();
+    chip8RV[1]= 32;
+    decodeOpcode();
     equal(pc,4,"Skip as VX == NN");
 });
 test("0x4XNN", function() {
@@ -73,9 +73,9 @@ test("0x4XNN", function() {
     memory = new Array();
     memory[0] = '0x41';
     memory[1] = '0x21';
-    clearAllRegisters();
-    chip8_rv[1]= 32;
-    emulateCycle();
+    clearRegisters();
+    chip8RV[1]= 32;
+    decodeOpcode();
     equal(pc,4,"Skip as VX != NN");
 });
 test("0x5XYO", function() {
@@ -83,10 +83,10 @@ test("0x5XYO", function() {
     memory = new Array();
     memory[0] = '0x51';
     memory[1] = '0x20';
-    clearAllRegisters();
-    chip8_rv[1]= 32;
-    chip8_rv[2]= 32;
-    emulateCycle();
+    clearRegisters();
+    chip8RV[1]= 32;
+    chip8RV[2]= 32;
+    decodeOpcode();
     equal(pc,4,"Skip as VX == VY");
 });
 test("0x6XNN", function() {
@@ -94,9 +94,9 @@ test("0x6XNN", function() {
     memory = new Array();
     memory[0] = '0x61';
     memory[1] = '0x20';
-    clearAllRegisters();
-    emulateCycle();
-    equal(chip8_rv[1],32,"Set VX to NN");
+    clearRegisters();
+    decodeOpcode();
+    equal(chip8RV[1],32,"Set VX to NN");
     equal(pc,2,"Increment pc");
 });
 test("0x7XNN", function() {
@@ -104,10 +104,10 @@ test("0x7XNN", function() {
     memory = new Array();
     memory[0] = '0x71';
     memory[1] = '0x20';
-    clearAllRegisters();
-    chip8_rv[1] = 32;
-    emulateCycle();
-    equal(chip8_rv[1],64,"Add NN to VX"); 
+    clearRegisters();
+    chip8RV[1] = 32;
+    decodeOpcode();
+    equal(chip8RV[1],64,"Add NN to VX"); 
     equal(pc,2,"Increment pc");
 });
 test("0x8XY0", function() {
@@ -115,11 +115,11 @@ test("0x8XY0", function() {
     memory = new Array();
     memory[0] = '0x81';
     memory[1] = '0x20';
-    clearAllRegisters();
-    chip8_rv[1] = 32;
-    chip8_rv[2] = 34;
-    emulateCycle();
-    equal(chip8_rv[1], 34,"Set VX to VY");
+    clearRegisters();
+    chip8RV[1] = 32;
+    chip8RV[2] = 34;
+    decodeOpcode();
+    equal(chip8RV[1], 34,"Set VX to VY");
     equal(pc,2,"Increment pc");
 });
 test("0x8XY1", function() {
@@ -127,11 +127,11 @@ test("0x8XY1", function() {
     memory = new Array();
     memory[0] = '0x81';
     memory[1] = '0x21';
-    clearAllRegisters();
-    chip8_rv[1] = 32;
-    chip8_rv[2] = 34;
-    emulateCycle();
-    equal(chip8_rv[1], 34,"Set VX to VX or VY");
+    clearRegisters();
+    chip8RV[1] = 32;
+    chip8RV[2] = 34;
+    decodeOpcode();
+    equal(chip8RV[1], 34,"Set VX to VX or VY");
     equal(pc,2,"Increment pc");
 });
 test("0x8XY2", function() {
@@ -139,11 +139,11 @@ test("0x8XY2", function() {
     memory = new Array();
     memory[0] = '0x81';
     memory[1] = '0x22';
-    clearAllRegisters();
-    chip8_rv[1] = 32;
-    chip8_rv[2] = 34;
-    emulateCycle();
-    equal(chip8_rv[1], 32,"Set VX to VX and VY");
+    clearRegisters();
+    chip8RV[1] = 32;
+    chip8RV[2] = 34;
+    decodeOpcode();
+    equal(chip8RV[1], 32,"Set VX to VX and VY");
     equal(pc,2,"Increment pc");
 });
 test("0x8XY3", function() {
@@ -151,10 +151,10 @@ test("0x8XY3", function() {
     memory = new Array();
     memory[0] = '0x81';
     memory[1] = '0x23';
-    clearAllRegisters();
-    chip8_rv[1] = 32;
-    chip8_rv[2] = 34;
-    emulateCycle();
+    clearRegisters();
+    chip8RV[1] = 32;
+    chip8RV[2] = 34;
+    decodeOpcode();
     equal(pc,2,"Increment pc");
 });
 test("0x8XY4", function() {
@@ -162,12 +162,12 @@ test("0x8XY4", function() {
     memory = new Array();
     memory[0] = '0x81';
     memory[1] = '0x24';
-    clearAllRegisters();
-    chip8_rv[1] = 32;
-    chip8_rv[2] = 34;
-    emulateCycle();
-    equal(chip8_rv[1], 66,"Add VY to VX");
-    equal(chip8_rv[0xF], 0, "Set carry bit");
+    clearRegisters();
+    chip8RV[1] = 32;
+    chip8RV[2] = 34;
+    decodeOpcode();
+    equal(chip8RV[1], 66,"Add VY to VX");
+    equal(chip8RV[0xF], 0, "Set carry bit");
     equal(pc,2,"Increment pc");
 });
 test("0x8XY4 with carry", function() {
@@ -175,12 +175,12 @@ test("0x8XY4 with carry", function() {
     memory = new Array();
     memory[0] = '0x81';
     memory[1] = '0x24';
-    clearAllRegisters();
-    chip8_rv[1] = 250;
-    chip8_rv[2] = 250;
-    emulateCycle();
-    equal(chip8_rv[1], 500,"Add VY to VX with carry");
-    equal(chip8_rv[0xF], 1, "Set carry bit");
+    clearRegisters();
+    chip8RV[1] = 250;
+    chip8RV[2] = 250;
+    decodeOpcode();
+    equal(chip8RV[1], 500,"Add VY to VX with carry");
+    equal(chip8RV[0xF], 1, "Set carry bit");
     equal(pc,2,"Increment pc");
 });
 
@@ -189,12 +189,12 @@ test("0x8XY5", function() {
     memory = new Array();
     memory[0] = '0x81';
     memory[1] = '0x25';
-    clearAllRegisters();
-    chip8_rv[1] = 34;
-    chip8_rv[2] = 32;
-    emulateCycle();
-    equal(chip8_rv[1], 2,"Subtract VY from VX");
-    equal(chip8_rv[0xF], 1, "Set lack of borrow");
+    clearRegisters();
+    chip8RV[1] = 34;
+    chip8RV[2] = 32;
+    decodeOpcode();
+    equal(chip8RV[1], 2,"Subtract VY from VX");
+    equal(chip8RV[0xF], 1, "Set lack of borrow");
     equal(pc,2,"Increment pc");
 });
 test("0x8XY5 with borrow", function() {
@@ -202,12 +202,12 @@ test("0x8XY5 with borrow", function() {
     memory = new Array();
     memory[0] = '0x81';
     memory[1] = '0x25';
-    clearAllRegisters();
-    chip8_rv[1] = 32;
-    chip8_rv[2] = 34;
-    emulateCycle();
-    equal(chip8_rv[1], -2,"Subtract VY from VX");
-    equal(chip8_rv[0xF], 0, "Set borrow bit");
+    clearRegisters();
+    chip8RV[1] = 32;
+    chip8RV[2] = 34;
+    decodeOpcode();
+    equal(chip8RV[1], -2,"Subtract VY from VX");
+    equal(chip8RV[0xF], 0, "Set borrow bit");
     equal(pc,2,"Increment pc");
 });
 test("0x8XY6", function() {
@@ -215,11 +215,11 @@ test("0x8XY6", function() {
     memory = new Array();
     memory[0] = '0x81';
     memory[1] = '0x26';
-    clearAllRegisters();
-    chip8_rv[1] = 32;
-    emulateCycle();
-    equal(chip8_rv[1], 16,"Shift VX right");
-    equal(chip8_rv[0xF], 0, "Set overflow bit");
+    clearRegisters();
+    chip8RV[1] = 32;
+    decodeOpcode();
+    equal(chip8RV[1], 16,"Shift VX right");
+    equal(chip8RV[0xF], 0, "Set overflow bit");
     equal(pc,2,"Increment pc");
 });
 test("0x8XY7", function() {
@@ -227,13 +227,13 @@ test("0x8XY7", function() {
     memory = new Array();
     memory[0] = '0x81';
     memory[1] = '0x27';
-    chip8_rv = new Array();
-    clearAllRegisters();
-    chip8_rv[1] = 32;
-    chip8_rv[2] = 34;
-    emulateCycle();
-    equal(chip8_rv[1], 2,"Set VX to VY - VX");
-    equal(chip8_rv[0xF], 1, "Set carry bit");
+    chip8RV = new Array();
+    clearRegisters();
+    chip8RV[1] = 32;
+    chip8RV[2] = 34;
+    decodeOpcode();
+    equal(chip8RV[1], 2,"Set VX to VY - VX");
+    equal(chip8RV[0xF], 1, "Set carry bit");
     equal(pc,2,"Increment pc");
 });
 test("0x8XY7 with borrow", function() {
@@ -241,13 +241,13 @@ test("0x8XY7 with borrow", function() {
     memory = new Array();
     memory[0] = '0x81';
     memory[1] = '0x27';
-    chip8_rv = new Array();
-    clearAllRegisters();
-    chip8_rv[1] = 34;
-    chip8_rv[2] = 32;
-    emulateCycle();
-    equal(chip8_rv[1], -2,"Set VX to VY - VX");
-    equal(chip8_rv[0xF], 0, "Set carry bit");
+    chip8RV = new Array();
+    clearRegisters();
+    chip8RV[1] = 34;
+    chip8RV[2] = 32;
+    decodeOpcode();
+    equal(chip8RV[1], -2,"Set VX to VY - VX");
+    equal(chip8RV[0xF], 0, "Set carry bit");
     equal(pc,2,"Increment pc");
 });
 
@@ -256,11 +256,11 @@ test("0x8XYE", function() {
     memory = new Array();
     memory[0] = '0x81';
     memory[1] = '0x2E';
-    clearAllRegisters();
-    chip8_rv[1] = 32;
-    emulateCycle();
-    equal(chip8_rv[1], 64,"Shift VX left");
-    equal(chip8_rv[0xF], 0, "Set overflow bit");
+    clearRegisters();
+    chip8RV[1] = 32;
+    decodeOpcode();
+    equal(chip8RV[1], 64,"Shift VX left");
+    equal(chip8RV[0xF], 0, "Set overflow bit");
     equal(pc,2,"Increment pc");
 });
 test("0x9XY0", function() {
@@ -268,10 +268,10 @@ test("0x9XY0", function() {
     memory = new Array();
     memory[0] = '0x91';
     memory[1] = '0x20';
-    clearAllRegisters();
-    chip8_rv[1] = 32;
-    chip8_rv[2] = 33;
-    emulateCycle();
+    clearRegisters();
+    chip8RV[1] = 32;
+    chip8RV[2] = 33;
+    decodeOpcode();
     equal(pc,4,"Skip as VX != VY"); 
 });
 
@@ -281,7 +281,7 @@ test("0xA123", function() {
     memory = new Array();
     memory[0] = '0xA1';
     memory[1] = '0x23';
-    emulateCycle();
+    decodeOpcode();
     equal(I,291,"Set I to NNN"); 
     equal(pc,2,"Increment pc");
 });
@@ -291,9 +291,9 @@ test("0xBNNN", function() {
     memory = new Array();
     memory[0] = '0xB1';
     memory[1] = '0x20';
-    clearAllRegisters();
-    chip8_rv[0] = 32;
-    emulateCycle();
+    clearRegisters();
+    chip8RV[0] = 32;
+    decodeOpcode();
     equal(pc,320,"Jump to a new address + V0");
 });
 
@@ -302,7 +302,7 @@ test("0xCXNN", function() {
     memory = new Array();
     memory[0] = '0xC1';
     memory[1] = '0x20';
-    emulateCycle();
+    decodeOpcode();
     //How do you test random?!?
     equal(pc,2,"Increment pc");
 });
@@ -313,11 +313,11 @@ test("0xEX9E", function() {
     memory = new Array();
     memory[0] = '0xE1';
     memory[1] = '0x9E';
-    clearAllRegisters();
+    clearRegisters();
     key = new Array();
     key[0x8] = 1;
-    chip8_rv[1] = '0x8';   
-    emulateCycle();
+    chip8RV[1] = '0x8';   
+    decodeOpcode();
     equal(pc,4,"Skip if key in VX is pressed");
 });
 test("0xEXA1", function() {
@@ -325,11 +325,11 @@ test("0xEXA1", function() {
     memory = new Array();
     memory[0] = '0xE1';
     memory[1] = '0xA1';
-    clearAllRegisters();
+    clearRegisters();
     key = new Array();
     key[0x8] = 0;
-    chip8_rv[1] = '0x8';   
-    emulateCycle();
+    chip8RV[1] = '0x8';   
+    decodeOpcode();
     equal(pc,4,"Skip if key in VX isnt pressed");
 });
 
@@ -338,10 +338,10 @@ test("0xFX07", function() {
     memory = new Array();
     memory[0] = '0xF1';
     memory[1] = '0x07';
-    clearAllRegisters();
+    clearRegisters();
     delayTimer = 50;
-    emulateCycle();
-    equal(chip8_rv[1], 50, "Set VX to delay timer");
+    decodeOpcode();
+    equal(chip8RV[1], 50, "Set VX to delay timer");
     equal(pc,2,"Increment pc");
 });
 
@@ -351,10 +351,10 @@ test("0xFX15", function() {
     memory = new Array();
     memory[0] = '0xF1';
     memory[1] = '0x15';
-    clearAllRegisters();
+    clearRegisters();
     delayTimer = 0;
-    chip8_rv[1] = 25;
-    emulateCycle();
+    chip8RV[1] = 25;
+    decodeOpcode();
     equal(delayTimer,25, "Set delay timer to VX");
     equal(pc,2,"Increment pc");
 });
@@ -364,10 +364,10 @@ test("0xFX18", function() {
     memory = new Array();
     memory[0] = '0xF1';
     memory[1] = '0x18';
-    clearAllRegisters();
+    clearRegisters();
     soundTimer = 0;
-    chip8_rv[1] = 25;
-    emulateCycle();
+    chip8RV[1] = 25;
+    decodeOpcode();
     equal(soundTimer,25, "Set sound timer to VX");
     equal(pc,2,"Increment pc");
 });
@@ -378,9 +378,9 @@ test("0xFX1E", function() {
     memory = new Array();
     memory[0] = '0xF1';
     memory[1] = '0x1E';
-    clearAllRegisters();
-    chip8_rv[1] = 30;
-    emulateCycle();
+    clearRegisters();
+    chip8RV[1] = 30;
+    decodeOpcode();
     equal(I,31,"Set I = VX + I");
     equal(pc,2,"Increment pc");
 });
@@ -394,13 +394,13 @@ test("0xFX55", function() {
     memory = new Array();
     memory[0] = '0xF4';
     memory[1] = '0x55';
-    clearAllRegisters();
-    chip8_rv[0] = 30;
-    chip8_rv[1] = 60;
-    chip8_rv[2] = 90;
-    chip8_rv[3] = 100;
-    chip8_rv[4] = 120;
-    emulateCycle();
+    clearRegisters();
+    chip8RV[0] = 30;
+    chip8RV[1] = 60;
+    chip8RV[2] = 90;
+    chip8RV[3] = 100;
+    chip8RV[4] = 120;
+    decodeOpcode();
     equal(memory[6],120,"Stores V0 to VX in memory starting at address I");
     equal(pc,2,"Increment pc");
 });
@@ -411,11 +411,11 @@ test("0xFX65", function() {
     memory = new Array();
     memory[0] = '0xF2';
     memory[1] = '0x65';
-    clearAllRegisters();
+    clearRegisters();
     memory[2] = 10;
     memory[3] = 20;
     memory[4] = 30;
-    emulateCycle();
-    equal(chip8_rv[1], 20, "Fill V0 to VX from I");
+    decodeOpcode();
+    equal(chip8RV[1], 20, "Fill V0 to VX from I");
     equal(pc,2,"Increment pc");
 });
